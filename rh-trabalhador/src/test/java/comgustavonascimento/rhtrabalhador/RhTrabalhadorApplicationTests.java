332ae73af1d4a2c96c2960e1da4394ca0149f942
Gustavo Henrique <gustavohgustavo@hotmail.com>
2023-01-10 12:14:22 -0300

    Rh-trabalho criado

---
+++ rh-trabalhador/src/test/java/comgustavonascimento/rhtrabalhador/RhTrabalhadorApplicationTests.java
@@ -0,0 +1,13 @@
+package comgustavonascimento.rhtrabalhador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhTrabalhadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
