4c82adadd328b5d80a4420b683c83e0746b3847b
Gustavo Henrique <gustavohgustavo@hotmail.com>
2023-01-11 10:26:51 -0300

    Rh-folha de pagamento criado

---
+++ rh-folhadepagamento/src/main/java/comgustavonascimento/rhfolhadepagamento/RhFolhadepagamentoApplication.java
@@ -0,0 +1,13 @@
+package comgustavonascimento.rhfolhadepagamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhFolhadepagamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhFolhadepagamentoApplication.class, args);
+	}
+
+}
