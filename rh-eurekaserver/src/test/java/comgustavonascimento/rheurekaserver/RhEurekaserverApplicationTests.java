39d39135c49c443621bcdb7748b05b3b34e9165b
Gustavo Henrique <gustavohgustavo@hotmail.com>
2023-01-11 15:39:02 -0300

    Projeto rh-eureka server criado

---
+++ rh-eurekaserver/src/test/java/comgustavonascimento/rheurekaserver/RhEurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package comgustavonascimento.rheurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhEurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
