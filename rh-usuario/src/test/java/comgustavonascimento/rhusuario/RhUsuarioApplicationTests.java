522c5506befe6f0c0dc575cebec06c46447fc6a8
Gustavo Henrique <gustavohgustavo@hotmail.com>
2023-01-13 17:26:55 -0300

    Projeto rh-usuário criado

---
+++ rh-usuario/src/test/java/comgustavonascimento/rhusuario/RhUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package comgustavonascimento.rhusuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
