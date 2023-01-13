a978fa5c02bd7d2370b667e6004073844b19d0f8
Gustavo Henrique <gustavohgustavo@hotmail.com>
2023-01-13 09:34:39 -0300

    Projeto rh-config server

---
+++ rh-configserver/src/main/java/comgustavonascimento/rhconfigserver/RhConfigserverApplication.java
@@ -0,0 +1,13 @@
+package comgustavonascimento.rhconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhConfigserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhConfigserverApplication.class, args);
+	}
+
+}
---
+++ rh-configserver/src/test/java/comgustavonascimento/rhconfigserver/RhConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package comgustavonascimento.rhconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
