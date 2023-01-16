095b4109074cd5c2087128b15d0a0e7f07a685fa
Gustavo Henrique <gustavohgustavo@hotmail.com>
2023-01-16 10:34:44 -0300

    Projeto rh-oauth criado

---
+++ rh-oauth/src/test/java/comgustavonascimento/rhoauth/RhOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package comgustavonascimento.rhoauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
